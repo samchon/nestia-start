ddf75293a39e66bd5a384955dd974ed572e0ebcc
Jeongho Nam <samchon.github@gmail.com>
2022-10-16 16:10:29 +0900

    comments on fake controller

--- src/controllers/BbsArticlesController.ts
+++ src/controllers/BbsArticlesController.ts
@@ -1,5 +1,6 @@
-import helper from "nestia-helper";
 import { Controller } from "@nestjs/common";
+import helper from "nestia-helper";
+
 import { IBbsArticle } from "../api/structures/bbs/IBbsArticle";
 import { IPage } from "../api/structures/common/IPage";
 import { BbsArticleProvider } from "../providers/bbs/BbsArticleProvider";
@@ -11,6 +12,13 @@ import { BbsArticleProvider } from "../providers/bbs/BbsArticleProvider";
  */
 @Controller("bbs/articles/:section")
 export class BbsArticlesController {
+    /**
+     * List up entire articles, but paginated and summarized.
+     *
+     * @param section Target section
+     * @param input Pagination request info with searching and sorting options
+     * @returns Paged articles witb summarization
+     */
     @helper.EncryptedRoute.Patch()
     public index(
         @helper.TypedParam("section", "string") section: string,
@@ -19,6 +27,13 @@ export class BbsArticlesController {
         return BbsArticleProvider.index(section, input);
     }
 
+    /**
+     * Get an article with detailed info.
+     *
+     * @param section Target section
+     * @param id Target articles id
+     * @returns Detailed article info
+     */
     @helper.TypedRoute.Get(":id")
     public at(
         @helper.TypedParam("section", "string") section: string,
@@ -27,6 +42,13 @@ export class BbsArticlesController {
         return BbsArticleProvider.find(section, id);
     }
 
+    /**
+     * Store a new article.
+     *
+     * @param section Target section
+     * @param input New article info
+     * @returns Newly created article info
+     */
     @helper.TypedRoute.Post()
     public store(
         @helper.TypedParam("section", "string") section: string,
@@ -35,6 +57,18 @@ export class BbsArticlesController {
         return BbsArticleProvider.store(section, input);
     }
 
+    /**
+     * Update article.
+     *
+     * When updating, this BBS system does not overwrite the content, but accumulate it.
+     * Therefore, whenever an article being updated, length of {@link IBbsArticle.contents}
+     * would be increased and accumulated.
+     *
+     * @param section Target section
+     * @param id Target articles id
+     * @param input Content to update
+     * @returns Newly created content info
+     */
     @helper.TypedRoute.Put(":id")
     public update(
         @helper.TypedParam("section", "string") section: string,
