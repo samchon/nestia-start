a363bf1a929ee0acf46e2c2c44a59bf522b6e105
Jeongho Nam <samchon@wrtn.io>
2023-10-20 16:05:31 +0900

    MyConfiguration.ROOT

--- src/MyConfiguration.ts
+++ src/MyConfiguration.ts
@@ -4,7 +4,14 @@ export namespace MyConfiguration {
     export const API_PORT = () => 37001;
 
     export const ROOT = __filename.includes(
-        path.join("bin", "src", "ShoppingConfiguration"),
+        path.join(
+            "bin",
+            "src",
+            (() => {
+                const name: string = __filename.split(path.sep).pop()!;
+                return name.substring(0, name.length - 3);
+            })(),
+        ),
     )
         ? path.resolve(__dirname + "/../..")
         : path.resolve(__dirname + "/..");
