2fb659b4ade9b92f830da4c6517659938a7ffd85
Jeongho Nam <samchon.github@gmail.com>
2024-01-23 16:58:36 +0900

    Fix eslint bug

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,23 @@
 module.exports = {
-    root: true,
-    plugins: [
-        "@typescript-eslint",
-        "deprecation",
-    ],
-    extends: [
-        "plugin:@typescript-eslint/recommended",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: [
-            "tsconfig.json",
-            "test/tsconfig.json",
-        ]
+  root: true,
+  plugins: ["@typescript-eslint", "deprecation"],
+  extends: ["plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "test/tsconfig.json"],
+  },
+  overrides: [
+    {
+      files: ["src/**/*.ts", "test/**/*.ts"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "deprecation/deprecation": "error",
+      },
     },
-    overrides: [
-        {
-            files: ["src/**/*.ts", "test/**/*.ts"],
-            rules: {
-                "@typescript-eslint/no-duplicate-imports": "error",
-                "@typescript-eslint/no-explicit-any": "off",
-                "@typescript-eslint/no-floating-promises": "error",
-                "@typescript-eslint/no-duplicate-imports": "off",
-                "@typescript-eslint/no-inferrable-types": "off",
-                "@typescript-eslint/no-namespace": "off",
-                "@typescript-eslint/no-non-null-assertion": "off",
-                "@typescript-eslint/no-var-requires": "off",
-                "deprecation/deprecation": "error",
-            }
-        }
-    ]
+  ],
 };
