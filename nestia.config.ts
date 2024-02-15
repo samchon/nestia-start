a5c6b1e63d0d06d314e5494c83ae1fb6093b4925
Jeongho Nam <samchon.github@gmail.com>
2024-02-16 00:14:42 +0900

    Upgrade dependencies

--- nestia.config.ts
+++ nestia.config.ts
@@ -5,19 +5,20 @@ import { NestFactory } from "@nestjs/core";
 import { MyModule } from "./src/MyModule";
 
 const NESTIA_CONFIG: sdk.INestiaConfig = {
-    input: () => NestFactory.create(MyModule),
-    output: "src/api",
-    swagger: {
-        output: "packages/api/swagger.json",
-        servers: [
-            {
-                url: "http://localhost:37001",
-                description: "Local Server",
-            },
-        ],
-    },
-    distribute: "packages/api",
-    simulate: true,
-    e2e: "test",
+  input: () => NestFactory.create(MyModule),
+  output: "src/api",
+  swagger: {
+    output: "packages/api/swagger.json",
+    servers: [
+      {
+        url: "http://localhost:37001",
+        description: "Local Server",
+      },
+    ],
+    beautify: true,
+  },
+  distribute: "packages/api",
+  primitive: false,
+  simulate: true,
 };
 export default NESTIA_CONFIG;
