a0c78c63374ac1dde12f8a0dbdd73762446cc51f
Jeongho Nam <samchon.github@gmail.com>
2026-07-10 01:06:22 +0900

    resolved?

--- packages/api/lint.config.ts
+++ packages/api/lint.config.ts
@@ -1,7 +1,7 @@
 import type { ITtscLintConfig } from "@ttsc/lint";
 
 export default async () => {
-  const shared = (await import("@ORGANIZATION/PROJECT-config/lint")).default;
+  const shared = (await import("@ORGANIZATION/PROJECT-config/lint.config.ts")).default;
   return {
     ...shared,
     ignores: ["src/functional/**/*.ts"],
--- packages/backend/test/lint.config.ts
+++
@@ -1,8 +0,0 @@
-import type { ITtscLintConfig } from "@ttsc/lint";
-
-export default async () => {
-  const shared = (await import("@ORGANIZATION/PROJECT-config/lint")).default;
-  return {
-    ...shared,
-  } satisfies ITtscLintConfig;
-};
